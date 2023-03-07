9364344ee848916c56c7ae646850b8bb0cdcc67e
DJ Keiran <100867528+keiranchippendaleuk@users.noreply.github.com>
2023-03-07 21:03:32 +0000

    added a little more security

--- request.php
+++ request.php
@@ -2,11 +2,27 @@
 /*
 made by: djkeiran.co.uk
 last updated: 07/03/23
+time: 21:03
 */
 
-$title = isset($_GET['title']) ? htmlspecialchars($_GET['title'], ENT_QUOTES | ENT_HTML5, 'UTF-8') : '';
-$artist = isset($_GET['artist']) ? htmlspecialchars($_GET['artist'], ENT_QUOTES | ENT_HTML5, 'UTF-8') : '';
-$message = isset($_GET['message']) ? htmlspecialchars($_GET['message'], ENT_QUOTES | ENT_HTML5, 'UTF-8') : '';
+$title = isset($_GET['title']) ? trim($_GET['title']) : '';
+$artist = isset($_GET['artist']) ? trim($_GET['artist']) : '';
+$message = isset($_GET['message']) ? trim($_GET['message']) : '';
+
+// Validate input
+if (empty($title) || empty($artist)) {
+  header('Content-Type: application/json');
+  echo json_encode(array(
+    'success' => false,
+    'message' => "Title and artist parameters are required",
+  ));
+  exit;
+}
+
+// Prevent XSS attacks
+$title = htmlspecialchars($title, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+$artist = htmlspecialchars($artist, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+$message = htmlspecialchars($message, ENT_QUOTES | ENT_HTML5, 'UTF-8');
 
 $searchUrl = "https://api.deezer.com/2.0/search?q=" . urlencode($title . ' ' . $artist);
 $searchResult = json_decode(file_get_contents($searchUrl), true);
